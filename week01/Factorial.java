45c4ac3d36a3a2e80c463046dce972985f495be2
cobtech_msb <cobtmsb@gmail.com>
2014-09-12 03:58:18 +0000

    week02 temporary

---
+++ week01/Factorial.java
@@ -0,0 +1,14 @@
+class Factorial {
+    private static int factorial(int i) {
+        if (i == 0 || i == 1)
+            return 1;
+        else
+            return i * factorial(i - 1);
+    }
+    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + "      " + factorial(i));
+        }
+    }
+}
